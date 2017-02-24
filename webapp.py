da185a7ba207dbe5ce1de6aaf560fef037e4b280
Victor Manuel Tenorio <bossipo@debian-Bossipo.portatil-Bossipo.com>
2017-02-24 23:09:17 +0100

    14.6: URLs aleatorias con interfaz webapp

--- webapp.py
+++ webapp.py
@@ -28,12 +28,11 @@ class webApp:
 
     def process(self, parsedRequest):
         """Process the relevant elements of the request.
-
         Returns the HTTP code for the reply, and an HTML page.
         """
 
         return ("200 OK", "<html><body><h1>It works!</h1></body></html>")
-
+        
     def __init__(self, hostname, port):
         """Initialize the web application."""
 
